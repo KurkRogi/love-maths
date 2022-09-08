0df36760dcc09121077334f9e0d7ce2dc7518301
KurkRogi <igor.k.kruk@gmail.com>
2022-09-08 20:44:18 +0000

    Add fav icon and tidy up the code adding user friendly features

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer()
+        }
+    })
+
     runGame("addition");
 });
 
@@ -27,6 +33,9 @@ function runGame(gameType) {
     //Create 2 random numbers
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
